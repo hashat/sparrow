ea1736b2a25b02a70def239f61d32dba2752e42d
hashat <hashat@sent.at>
2021-03-28 12:54:37 +0200

    SortedMulti should now be ready, albeit tried only for receiving.

--- src/main/java/com/sparrowwallet/sparrow/event/WalletAddressesChangedEvent.java
+++ src/main/java/com/sparrowwallet/sparrow/event/WalletAddressesChangedEvent.java
@@ -5,7 +5,7 @@ import com.sparrowwallet.drongo.wallet.Wallet;
 import java.io.File;
 
 /**
- * This event is posted when a wallet's addresses are changed (keystores, policy, script type).
+ * This event is posted when a wallet's addresses are changed (keystores, policy, script type, sortedness, wallet chains).
  * This event marks a fundamental change that is used to update application level UI, clear node entry caches and similar. It should only be subscribed to by application-level classes.
  * Note that WalletForm does not listen to this event to save the wallet, since the wallet is foreground saved directly in SettingsController before this event is posted.
  * This is because any failure in saving the wallet must be immediately reported to the user.
